dba682483ad940f7ea84e983d1a54481850e23bd
simsekali38 <bozkurtt1997@outlook.com>
2021-08-18 09:25:20 +0300

    finance update

--- database/migrations/2021_08_39_181120_finance_table.php
+++ database/migrations/2021_08_39_181120_finance_table.php
@@ -16,6 +16,17 @@ class FinanceTable extends Migration
         Schema::create('finance', function (Blueprint $table) {
             $table->increments('id');
             $table->bigInteger('user_id')->unsigned();
+            $table->string('name');
+            $table->decimal('amount')->comment('Üyenin çekeceği miktar');
+            $table->decimal('first_payment_amount')->comment('Ödeme öncesi taksit miktarı');
+            $table->integer('get_month')->comment('paranın alınacağı ay');
+            $table->decimal('amount_payable')->comment('Üyenin ödeyeceği miktar');
+            $table->decimal('remaining_amount')->comment('Kalan ödeme miktarı');
+            $table->decimal('last_payment_amount')->comment('Ödeme sonrası taksit miktarı');
+            $table->integer('number_of_payments')->comment('Taksit sayısı');
+            $table->integer('participation_fee')->comment('Katılım masrafı');
+            $table->decimal('deposit')->comment('Depozito');
+            $table->text('description')->comment('Açıklama');
 
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')
