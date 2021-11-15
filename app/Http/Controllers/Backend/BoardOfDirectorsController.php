e41c3ada4b665abdd5671505b35197c9714c1eae
hayriozblbl <hayriozbulbul@gmail.com>
2021-11-15 21:13:49 +0300

    update

--- app/Http/Controllers/Backend/BoardOfDirectorsController.php
+++ app/Http/Controllers/Backend/BoardOfDirectorsController.php
@@ -161,22 +161,48 @@ class BoardOfDirectorsController extends Controller
            $formFileName = "image";
            $fileFinalName = "";
 
-           if ($request->$formFileName != "") {
-               $teamx = BoardofDirectors::find($id);  // here to store image alone
-               $fileFinalName = time() . rand(1111, 9999) . '.' . $request->file($formFileName)->getClientOriginalExtension();
-               $path = $this->getUploadPath();
-               $request->file($formFileName)->move($path, $fileFinalName);
-               $teamx->image = $fileFinalName; // here there is  a bug when update profile image
-               $teamx->save();
-           }
+        if ($request->hasFile('image')) {
 
-  
-  
-           // End of Upload Files
-          $boardofdirectory = BoardofDirectors::where('id',$id)->update($request->except('_token','_method','image'));
-        // except image cus we handle it aboves
-  
-  
+            $activityx = BoardofDirectors::find($id);  // here to store image alone
+            // Delete a style_logo_en photo
+            if ($activityx->f_image != "") {
+                unlink('public/uploads/BoardofDirectors/' . $activityx->f_image);
+            }
+
+            $fileNameWithExt = $request->file('image')->getClientOriginalName();
+            // get file name
+            $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            // get extension
+            $extension = $request->file('image')->getClientOriginalExtension();
+
+            $fileNameToStore =  time() . '.' . $extension;
+            // upload
+
+            $path = $request->file('image')->move('public/uploads/BoardofDirectors', $fileNameToStore);
+
+            $activityx->f_image = $fileNameToStore;
+            $activityx->save();
+        }
+
+
+// Start of Upload Files
+        if ($request->hasFile('BoardofDirectors')) {
+            $all_images = $request->file('BoardofDirectors');
+            $path = $this->getUploadPath();
+            foreach ($all_images as $file) {
+                $image_name = time() . rand(1111, 9999) . '.' . $file->getClientOriginalExtension();
+                $file->move($path, $image_name);
+                $activity_images = new BoardofDirectors;
+                $activity_images->activity_id = $id;
+                $activity_images->activity_image_path = $image_name;
+                $activity_images->save();
+            }
+        }
+        $activity->activity_types()->sync($request->activity_type);
+
+        $activity->unit_types()->sync($request->unit_type);
+
+
           return redirect(route('admin.boardofdirectory.index'))->with('message', trans('backend.updated_successfully'));
   
   
