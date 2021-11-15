d589511a3611b880f4ac9208a5a0111d715a0a0c
hayriozblbl <hayriozbulbul@gmail.com>
2021-11-16 00:37:29 +0300

    adverts upload fix

--- app/Http/Controllers/Backend/AdvertController.php
+++ app/Http/Controllers/Backend/AdvertController.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\File;
 class AdvertController extends BaseBackendController
 {
 
-    private $uploadPath = "uploads/galleries/";
+    private $uploadPath = "public/uploads/adverts/";
     /**
      * Display a listing of the resource.
      *
@@ -160,7 +160,7 @@ class AdvertController extends BaseBackendController
 
         $gallery_images = advert_images::where('gallery_id', $id)->get();
         foreach ($gallery_images as $image) {
-            unlink('public/uploads/galleries/' . $image->gallery_image_path);
+            unlink('public/uploads/adverts/' . $image->gallery_image_path);
         }
         Advert::where('id', $id)->delete();
         return redirect()->back();
